7d29de57aba07217643f1292be04f2b81139e92f
Scrappy1cocco <jedediah.shine100@gmail.com>
2019-01-23 19:11:52 +0300

    start bshPRO

--- TESTscript.js
+++ TESTscript.js
@@ -1,29 +1,60 @@
 "use strict";
 
-var test1 = "abcdef";
-var test2 = 123;
-var test3 = true;
-var test4 = {};
-var test5 = [];
-var test6;
-var test7 = {"abcdef": 123} ;
-var test8 = ["abcdef", 123];
-function test9() {return "abcdef"};
-var test10 = null;
+function lieDetectorTest() {
+	var lies = 0;
 
-console.log(typeof test1);
-console.log(typeof test2);
-console.log(typeof test3);
-console.log(typeof test4);
-console.log(typeof test5);
-console.log(typeof test6);
-console.log(typeof test7);
-console.log(typeof test8);
-console.log(typeof test9);
-console.log(typeof test10);
-
-if (99 == "99") {
-	console.log("A number equals a string!");
-} else {
-	console.log("No way a number equals a string");
+	var stolenDiamond = { };
+	if (stolenDiamond) {
+		console.log("You stole the diamond");
+		lies++;
+	}
+	var car = {
+		keysInPocket: null
+	}
+	if (car.keysInPocket) {
+		console.log("Uh oh, guess you stole the car!");
+		lies++;
+	}
+	if (car.emptyGasTank) {
+		console.log("You drove the car after you stole it!")
+		lies++;
+	}
+	var foundYouAtTheCrimeScene = [ ];
+	if (foundYouAtTheCrimeScene) {
+		console.log("A sure sign of guilt");
+		lies++;
+	}
+	if (foundYouAtTheCrimeScene[0]) {
+		console.log("Caught with a stolen item!");
+		lies++;
+	}
+	var yourName = " ";
+	if (yourName) {
+		console.log("Guess you lied about your name");
+		lies++;
+	}
+	return lies;
 }
+var numberOfLies = lieDetectorTest();
+console.log("You told " + numberOfLies + " lies!");
+if (numberOfLies >= 3) {
+	console.log("Guilty as charged");
+}
+
+var data = "name|phone|address";
+var vals = data.split("|");
+console.log("Split array is ", vals);
+
+function Duck(sound) {
+	this.sound = sound;
+	this.quack = function() {
+		console.log(this.sound);
+	}
+}
+
+var toy = new Duck("quack quack");
+
+toy.quack();
+
+console.log(typeof toy);
+console.log(toy instanceof Duck);
